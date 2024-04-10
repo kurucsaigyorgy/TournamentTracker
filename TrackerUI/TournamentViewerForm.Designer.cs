cc413b1f67903836b2118b97b3f8b21c2997c777
Kurucsai György <kurucsai.gyorgy@gmail.com>
2024-04-10 18:53:25 +0200

    cleanup forms, add logo and picture again

--- TrackerUI/TournamentViewerForm.Designer.cs
+++ TrackerUI/TournamentViewerForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace TournamentUI
+﻿namespace TrackerUI
 {
-    partial class TournamentViewerForm
+    partial class Form1
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            SuspendLayout();
-            // 
-            // TournamentViewerForm
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "TournamentViewerForm";
-            Text = "Tournament Viewer";
-            Load += Form1_Load;
-            ResumeLayout(false);
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
         }
 
         #endregion
