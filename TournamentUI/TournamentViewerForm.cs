d213744600fd8a74a267f1b1bb13182ad012e2a4
Kurucsai György <kurucsai.gyorgy@gmail.com>
2024-04-10 18:08:20 +0200

    create windows forms application to build Tracker UI

---
+++ TournamentUI/TournamentViewerForm.cs
@@ -0,0 +1,15 @@
+namespace TournamentUI
+{
+    public partial class TournamentViewerForm : Form
+    {
+        public TournamentViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
